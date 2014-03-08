9c8aa4c0bfea3bfb9b7235aee571f9efff0d85d6
Tatiana Kerick <KerickTatiana@gmail.com>
2014-03-07 23:31:28 -0500

    did lost of stuff throught the day and lost track. working on a modal

---
+++ Site/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+	jQuery(function ($) {
+		$('#signInLink').click(function (e) {
+			$('#signInDiv').modal();
+			return false;
+		})
+	});//jquery function
+
+})//end document
